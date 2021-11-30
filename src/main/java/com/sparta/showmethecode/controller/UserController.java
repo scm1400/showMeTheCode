d1c355e1d40b799de76744976e57007941bc541e
kimdh-hi <zbeld123@gmail.com>
2021-11-30 13:52:03 +0900

    리뷰어 관련 API 컨트롤러 분리

--- src/main/java/com/sparta/showmethecode/controller/UserController.java
+++ src/main/java/com/sparta/showmethecode/controller/UserController.java
@@ -77,45 +77,6 @@ public class UserController {
     }
 
     /**
-     * 나에게 요청된 리뷰요청목록 조회 API
-     */
-    @GetMapping("/user/reviewer/requests")
-    public ResponseEntity<ReviewRequestListResponseDto> getRequestedReviewList(
-            @AuthenticationPrincipal UserDetailsImpl userDetails
-    ) {
-
-        return null;
-    }
-
-    /**
-     * 리뷰요청에 대한 리뷰등록 API
-     */
-    @PostMapping("/user/reviewer/request")
-    public ResponseEntity addReviewAndComment(
-            @AuthenticationPrincipal UserDetailsImpl userDetails,
-            @RequestParam Long questionId,
-            @RequestBody AddReviewDto addReviewDto
-    ) {
-        User reviewer = userDetails.getUser();
-        reviewerService.addReviewAndComment(reviewer, questionId, addReviewDto);
-        return ResponseEntity.ok("ok");
-    }
-
-    /**
-     * 리뷰요청 거절 API
-     */
-    @GetMapping("/user/reviewer/request/reject")
-    public ResponseEntity rejectRequestedReview(
-            @AuthenticationPrincipal UserDetailsImpl userDetails,
-            @RequestParam Long questionId
-    ) {
-        User user = userDetails.getUser();
-        reviewerService.rejectRequestedReview(user, questionId);
-
-        return ResponseEntity.ok("ok");
-    }
-
-    /**
      * 답변에 대한 평가 API
      */
     @PostMapping("/user/question/{answerId}/eval")
@@ -128,31 +89,5 @@ public class UserController {
         userService.evaluateAnswer(user, answerId, evaluateAnswerDto);
 
         return ResponseEntity.ok("ok");
-    }
-
-    /**
-     * 리뷰어 랭킹 조회 API (전체랭킹 조회)
-     */
-    @GetMapping("/user/reviewer/rank")
-    public ResponseEntity getReviewerRanking(
-            @RequestParam(defaultValue = "1") int page,
-            @RequestParam(defaultValue = "10") int size,
-            @RequestParam(defaultValue = "true") boolean isAsc
-    ) {
-        --page;
-
-        PageResponseDto<ReviewerInfoDto> reviewerRanking = reviewerService.getReviewerRanking(page, size, isAsc);
-
-        return ResponseEntity.ok(reviewerRanking);
-    }
-
-    /**
-     * 리뷰어 랭킹 조회 API (상위 5위)
-     */
-    @GetMapping("/user/reviewer/top")
-    public ResponseEntity getReviewerTop5Ranking() {
-        List<ReviewerInfoDto> reviewers = reviewerService.getReviewerTop5Ranking();
-
-        return ResponseEntity.ok(reviewers);
     }
 }

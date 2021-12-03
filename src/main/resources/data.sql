a89460fded7baeabeb7da0cf80e813bb28ac5fe9
Muice <scm1400@gmail.com>
2021-12-03 16:02:33 +0900

    테스트데이터 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,12 @@
+insert into user(id, created_at, updated_at, answer_count, eval_count, eval_total, password, role, username)
+values (1, current_timestamp, current_timestamp, 1, 1, 1, 1, 'ROLE_USER', 'user1');
+insert into user(id, created_at, updated_at, answer_count, eval_count, eval_total, password, role, username)
+values (2, current_timestamp, current_timestamp, 1, 1, 1, 1, 'ROLE_USER', 'user2');
+
+insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
+values (1, current_timestamp, current_timestamp, '테스트내용', 'java', '미해결', '테스트제목', 2, 1);
+
+insert into review_answer(id, created_at, updated_at, content, point, title, answer_user_id, review_request_id)
+values (1, current_timestamp, current_timestamp, '테스트내용', 5, '테스트답변제목', 2, 1);
+
+update review_request set review_answer_id = 1 where id = 1

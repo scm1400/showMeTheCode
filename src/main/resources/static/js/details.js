18e4880d1c2a3726f63a9cc0348e000b88a53c85
Muice <scm1400@gmail.com>
2021-12-06 13:11:09 +0900

    #63 페이지네이션 수정, 상세페이지 제목, 날짜포맷 수정

--- src/main/resources/static/js/details.js
+++ src/main/resources/static/js/details.js
@@ -6,15 +6,18 @@ $(document).ready(function() {
         url: `/details?id=${id}`,
         contentType: "application/json;charset-utf-8;",
         success: function (res) {
-            console.log(res);
-            $("#request-title").html(res.title)
+            let date = new Date(res.createdAt)
+            let title = `<h1>`
+            title = title + res.title + `</h1>`
+            date = dateFormat(date)
+            $("#request-title").append(title)
             $("#user-name").html(res.username)
-            $("#created-at").html(res.createdAt)
+            $("#created-at").html(`&nbsp;· `+ date)
             $("#content").html(res.content)
             $("#sub-info__content").append(`<button class="ac-button is-sm is-solid is-gray  ac-tag ac-tag--blue "><span
                                                 class="ac-tag__hashtag">#&nbsp;</span><span
                                                 class="ac-tag__name">'${res.languageName}'</span></button>`)
-
+            let answer = res['reviewAnswerResponseDto']
             let comments = res['comments'];
         }
     })
@@ -27,4 +30,14 @@ function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"), results = regex.exec(location.search);
     return results == null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function dateFormat(date) {
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+
+    month = month >= 10 ? month : '0' + month;
+    day = day >= 10 ? day : '0' + day;
+
+    return date.getFullYear() + '.' + month + '.' + day;
 }

6fe2ab640e664df9bd17cfcd24e3588da0112e2d
Muice <scm1400@gmail.com>
2021-12-08 20:25:52 +0900

    #101 태그 클릭시 태그 검색결과 페이징

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -21,7 +21,7 @@ values(2, current_timestamp, current_timestamp, 'PYTHON', 4);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (1, current_timestamp, current_timestamp, '테스트내용', 'java', 'UNSOLVE', '테스트제목', 2, 1);
+values (1, current_timestamp, current_timestamp, '테스트내용', 'JAVA', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request_comment(id, created_at, updated_at, content,  review_request_id, user_id)
@@ -29,120 +29,120 @@ values (1, current_timestamp, current_timestamp, '테스트댓글내용', 1, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (2, current_timestamp, current_timestamp, '테스트내용', 'java', 'UNSOLVE', '테스트제목1 - java', 2, 1);
+values (2, current_timestamp, current_timestamp, '테스트내용', 'JAVA', 'UNSOLVE', '테스트제목1 - java', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (3, current_timestamp, current_timestamp, '테스트내용', 'java', 'UNSOLVE', '테스트제목2 - java', 2, 1);
+values (3, current_timestamp, current_timestamp, '테스트내용', 'JAVA', 'UNSOLVE', '테스트제목2 - java', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (4, current_timestamp, current_timestamp, '테스트내용', 'java', 'UNSOLVE', '테스트제목1 - python', 2, 1);
+values (4, current_timestamp, current_timestamp, '테스트내용', 'JAVA', 'UNSOLVE', '테스트제목1 - python', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (5, current_timestamp, current_timestamp, '테스트내용', 'java', 'UNSOLVE', '테스트제목2 - python', 2, 1);
+values (5, current_timestamp, current_timestamp, '테스트내용', 'JAVA', 'UNSOLVE', '테스트제목2 - python', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (6, current_timestamp, current_timestamp, '테스트내용', 'java', 'UNSOLVE', '테스트제목3 - spring', 2, 1);
+values (6, current_timestamp, current_timestamp, '테스트내용', 'JAVA', 'UNSOLVE', '테스트제목3 - spring', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (7, current_timestamp, current_timestamp, '테스트내용', 'java', 'UNSOLVE', '테스트제목4 - spring', 2, 1);
+values (7, current_timestamp, current_timestamp, '테스트내용', 'JAVA', 'UNSOLVE', '테스트제목4 - spring', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (8, current_timestamp, current_timestamp, '테스트내용 - 입력', 'java', 'UNSOLVE', '테스트제목', 2, 1);
+values (8, current_timestamp, current_timestamp, '테스트내용 - 입력', 'JAVA', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (9, current_timestamp, current_timestamp, '테스트내용 - 출력', 'java', 'UNSOLVE', '테스트제목', 2, 1);
+values (9, current_timestamp, current_timestamp, '테스트내용 - 출력', 'JAVA', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (10, current_timestamp, current_timestamp, '테스트내용', 'java', 'UNSOLVE', '테스트제목', 2, 1);
+values (10, current_timestamp, current_timestamp, '테스트내용', 'JAVA', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (11, current_timestamp, current_timestamp, '테스트내용', 'java', 'UNSOLVE', '테스트제목', 2, 1);
+values (11, current_timestamp, current_timestamp, '테스트내용', 'JAVA', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (12, current_timestamp, current_timestamp, '테스트내용', 'java', 'UNSOLVE', '테스트제목', 2, 1);
+values (12, current_timestamp, current_timestamp, '테스트내용', 'JAVA', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (13, current_timestamp, current_timestamp, '테스트내용', 'java', 'UNSOLVE', '테스트제목', 2, 1);
+values (13, current_timestamp, current_timestamp, '테스트내용', 'JAVA', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (14, current_timestamp, current_timestamp, '테스트내용', 'java', 'UNSOLVE', '테스트제목', 2, 1);
+values (14, current_timestamp, current_timestamp, '테스트내용', 'JAVA', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (15, current_timestamp, current_timestamp, '테스트내용', 'python', 'UNSOLVE', '테스트제목', 2, 1);
+values (15, current_timestamp, current_timestamp, '테스트내용', 'PYTHON', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (16, current_timestamp, current_timestamp, '테스트내용', 'python', 'UNSOLVE', '테스트제목', 2, 1);
+values (16, current_timestamp, current_timestamp, '테스트내용', 'PYTHON', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (17, current_timestamp, current_timestamp, '테스트내용', 'python', 'UNSOLVE', '테스트제목', 2, 1);
+values (17, current_timestamp, current_timestamp, '테스트내용', 'PYTHON', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (18, current_timestamp, current_timestamp, '테스트내용', 'python', 'UNSOLVE', '테스트제목', 2, 1);
+values (18, current_timestamp, current_timestamp, '테스트내용', 'PYTHON', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (19, current_timestamp, current_timestamp, '테스트내용', 'python', 'UNSOLVE', '테스트제목', 2, 1);
+values (19, current_timestamp, current_timestamp, '테스트내용', 'PYTHON', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (20, current_timestamp, current_timestamp, '테스트내용', 'python', 'UNSOLVE', '테스트제목', 2, 1);
+values (20, current_timestamp, current_timestamp, '테스트내용', 'PYTHON', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (21, current_timestamp, current_timestamp, '테스트내용', 'python', 'UNSOLVE', '테스트제목', 2, 1);
+values (21, current_timestamp, current_timestamp, '테스트내용', 'PYTHON', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (22, current_timestamp, current_timestamp, '테스트내용', 'python', 'UNSOLVE', '테스트제목', 2, 1);
+values (22, current_timestamp, current_timestamp, '테스트내용', 'PYTHON', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (23, current_timestamp, current_timestamp, '테스트내용', 'c', 'UNSOLVE', '테스트제목', 2, 1);
+values (23, current_timestamp, current_timestamp, '테스트내용', 'C', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (24, current_timestamp, current_timestamp, '테스트내용', 'c', 'UNSOLVE', '테스트제목', 2, 1);
+values (24, current_timestamp, current_timestamp, '테스트내용', 'C', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (25, current_timestamp, current_timestamp, '테스트내용', 'c', 'UNSOLVE', '테스트제목', 2, 1);
+values (25, current_timestamp, current_timestamp, '테스트내용', 'C', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (26, current_timestamp, current_timestamp, '테스트내용', 'c', 'UNSOLVE', '테스트제목', 2, 1);
+values (26, current_timestamp, current_timestamp, '테스트내용', 'C', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (27, current_timestamp, current_timestamp, '테스트내용', 'c', 'UNSOLVE', '테스트제목', 2, 1);
+values (27, current_timestamp, current_timestamp, '테스트내용', 'C', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (28, current_timestamp, current_timestamp, '테스트내용', 'c', 'UNSOLVE', '테스트제목', 2, 1);
+values (28, current_timestamp, current_timestamp, '테스트내용', 'C', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (29, current_timestamp, current_timestamp, '테스트내용', 'c', 'UNSOLVE', '테스트제목', 2, 1);
+values (29, current_timestamp, current_timestamp, '테스트내용', 'C', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (30, current_timestamp, current_timestamp, '테스트내용', 'c', 'UNSOLVE', '테스트제목', 2, 1);
+values (30, current_timestamp, current_timestamp, '테스트내용', 'C', 'UNSOLVE', '테스트제목', 2, 1);
 
 call next value for hibernate_sequence;
 insert into review_request(id, created_at, updated_at, content, language_name, status, title, answer_user_id,request_user_id)
-values (31, current_timestamp, current_timestamp, '테스트내용', 'c', 'UNSOLVE', '테스트제목', 2, 1);
+values (31, current_timestamp, current_timestamp, '테스트내용', 'C', 'UNSOLVE', '테스트제목', 2, 1);
--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -53,22 +53,15 @@ function getTag(){
     })
 }
 
-// function tagquery(tag){
-//     let query = "?query="+tag;
-//     location.href = query.toString();
-// }
 
 // ========================================
 // 언어별 코드리뷰 요청 목록보기
 // ========================================
-function getQuestionListByLanguage(language) {
+function getQuestionListByLanguage(language,page) {
     $('#reviewQuestionList').empty();
-    let currentPage = getParameterByName('page');
-    let query = getParameterByName('query');
+    $('#pagination-container').remove();
+    let currentPage = page;
 
-    if(!query){
-        query = null;
-    }
     if (!currentPage) {
         currentPage = 1
     }
@@ -81,7 +74,7 @@ function getQuestionListByLanguage(language) {
             language: language
         },
         success: function (res) {
-            makeQuestionList(res, currentPage);
+            makeQuestionListByLanguage(language, res, currentPage);
         }
     })
 }
@@ -141,9 +134,116 @@ function reviewSearch(){
     // location.href = "?query="+query
 }
 
+function makeQuestionListByLanguage(language,res, currentPage) {
+    let data = res['data']
+    let pagination = `<nav id="pagination-container" class="pagination is-centered is-small" role="navagation" aria-label="pagination">`
+    let totalpage = res.totalPage
+
+    // 전체 페이지가 1개인 경우
+    if (totalpage == 1) {
+        pagination += `
+                                    <ul class="pagination-list" id="pagingList">
+                                        <li>
+                                            <a class="pagination-link is-current" onclick="getQuestionListByLanguage('${language}',${1}) aria-label="1 페이지로 이동">
+                                                1
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </nav>
+                            `
+        // 전체 페이지가 1개 이상인 경우
+    } else {
+        if (currentPage < totalpage) {
+            pagination += `<a class="pagination-next" onclick="getQuestionListByLanguage('${language}',${nextPage})">다음 페이지</a>`
+        }
+        pagination += `<ul class="pagination-list" id="pagingList">`
+        for (let i = 1; i <= totalpage; i++) {
+            if (totalpage == 11) {
+                pagination += `<li>
+                            <a class="pagination-link" onclick="getQuestionListByLanguage('${language}',${i})" aria-label="${i} 페이지로 이동">
+                                ...
+                            </a>
+                        </li>`
+
+
+                break;
+            }
+            // 현재 페이지 버튼에 대한 처리
+            if (currentPage == i) {
+                pagination += `
+                                <li>
+                                    <a class="pagination-link is-current" onclick="getQuestionListByLanguage('${language}',${i})" aria-label="${i} 페이지로 이동">
+                                        ${i}
+                                    </a>
+                                </li>
+                                `
+            } else {
+                pagination += `
+                                <li>
+                                    <a class="pagination-link" onclick="getQuestionListByLanguage('${language}',${i})" aria-label="${i} 페이지로 이동">
+                                        ${i}
+                                    </a>
+                                </li>
+                                `
+            }
+
+        }
+        pagination += `</ul></nav>`
+    }
+    $('#community-body').append(pagination)
+
+    for (let i = 0; i < data.length; i++) {
+        let date = new Date(data[i].createdAt)
+        let content = data[i].content
+        content = content.toString().replace(/(<([^>]+)>)/ig,"").replace(/\r\n/g, "").slice(0,50)
+        date = dateFormat(date)
+        let li = `<li class="question-container">
+                                <a onclick="showQuestionDetails(${data[i].reviewRequestId})">
+                        <div class="question">
+                            <div class="question__info">
+                                        <div class="question__title">
+                                            <h3 class="title__text">
+                                                ${data[i].title}
+                                                <span class="infd-icon title__icon">
+                                                </span>
+                                            </h3>
+                                        </div>
+                                        <p class="question__body">
+                                            ${content}
+                                        </p>
+<!--                                        <div class="question__tags">-->
+<!--    -->
+<!--                                        </div>-->
+                                        <div class="question__info-footer">
+                                            ${data[i].languageName} · ${date}  · ${data[i].status} 
+                                        </div>
+                                    </div>
+                                    <div class="question__additional-info">
+                                        <div class="question__comment">
+                                            <span class="comment__count">${data[i].commentCount}</span>
+                                            <span class="comment__description">댓글수</span>
+                                        </div>
+    
+                                        <button class="ac-button is-md is-text question__like e-like">
+                                            <svg width="16" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                 viewBox="0 0 16 16">
+                                                <path fill="#616568"
+                                                      d="M9.333 13.605c-.328.205-.602.365-.795.473-.102.057-.205.113-.308.168h-.002c-.143.074-.313.074-.456 0-.105-.054-.208-.11-.31-.168-.193-.108-.467-.268-.795-.473-.655-.41-1.53-1.007-2.408-1.754C2.534 10.382.667 8.22.667 5.676c0-2.308 1.886-4.01 3.824-4.01 1.529 0 2.763.818 3.509 2.07.746-1.252 1.98-2.07 3.509-2.07 1.938 0 3.824 1.702 3.824 4.01 0 2.545-1.867 4.706-3.592 6.175-.878.747-1.753 1.344-2.408 1.754z"/>
+                                            </svg>
+                                            0
+                                        </button>
+                                    </div>
+                                </div>
+                            </a></li>`
+
+        $('#reviewQuestionList').append(li);
+
+    }
+}
+
 function makeQuestionList(res, currentPage) {
     let data = res['data']
-    let pagination = `<nav class="pagination is-centered is-small" role="navagation" aria-label="pagination">`
+    let pagination = `<nav id="pagination-container" class="pagination is-centered is-small" role="navagation" aria-label="pagination">`
     let totalpage = res.totalPage
 
     // 전체 페이지가 1개인 경우
